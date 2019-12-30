37c06fde8317875f586d72d3fc0138187a268cdc
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-30 11:33:06 -0300

    switch

--- src/conditional.php
+++ src/conditional.php
@@ -4,16 +4,11 @@ $number = 8;
 
 
 // If , elseif and else conditional
-if($number == 10)
-{
-   echo "It's equal 10";
-}
-elseif($number <=9)
-{
+if ($number == 10) {
+    echo "It's equal 10";
+} elseif ($number <= 9) {
     echo "It's less or than 9";
-}
-else
-{
+} else {
     echo "It's bigger";
 }
 
@@ -23,4 +18,27 @@ echo "<hr>";
 
 $average = 7;
 
-echo ($average >=7) ? "Aproved!" : "Reproved!";
+echo ($average >= 7) ? "Aproved!" : "Reproved!";
+
+
+// switch
+
+$color = "black";
+echo "<hr>";
+switch ($color) {
+    case "red":
+        echo "Your favorite color is red";
+        break;
+    case "green":
+        echo "Your favorite color is green";
+        break;
+    case "blue":
+        echo "Your favorite color is blue";
+        break;
+    case "white":
+        echo "Your favorite color is white";
+        break;
+    default:
+    echo "Your don't has a favorite color";
+        break;
+}
