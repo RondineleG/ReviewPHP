d21e013a6eefd936460ad95385850d95dc486502
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-30 10:37:59 -0300

    function is_array()

---
+++ src/array-functions.php
@@ -0,0 +1,19 @@
+<?php
+
+$names = array("Jose", "felipe", "Rondinele");
+
+
+// is_array();
+
+echo is_array($names);
+var_dump(is_array($names));
+echo "<hr>";
+
+if (is_array($names)) {
+    echo " is array";
+} else {
+    echo " is not array";
+}
+
+echo "<hr>";
+
