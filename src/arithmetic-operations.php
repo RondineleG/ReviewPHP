bbe8929a0e6d2fe7f4e54f629bade8c4996b42cc
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-30 11:44:48 -0300

    arithmetic operations

---
+++ src/arithmetic-operations.php
@@ -0,0 +1,46 @@
+<?php
+
+
+// arithmetic operations
+
+echo 10 + 41;
+echo"<hr>";
+echo 100 - 91;
+echo"<hr>";
+echo 8 * 89;
+echo"<hr>";
+echo 110 % 4;
+echo"<hr>";
+echo 3**5;
+
+$a = 10;
+$b = 20;
+$c =30;
+$d =40;
+
+echo"<hr>";
+$sum = $a + $c +$d;
+echo $sum;
+echo"<hr>";
+
+$subtraction = $d-$a;
+echo $subtraction;
+echo"<hr>";
+
+$multiplication = $b * $a;
+echo $multiplication;
+echo"<hr>";
+
+$division = $d / $a;
+echo $division;
+echo"<hr>";
+
+$module = $d / $c;
+echo $module;
+echo"<hr>";
+
+$exponentiation = $a ** $a;
+echo $exponentiation;
+echo"<hr>";
+
+echo 5 + 5 * (8 *10) - 4 /(10);
