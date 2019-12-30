59418afaec438741ef362c2238ff516d48bec7e4
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-30 11:25:35 -0300

    If , elseif and else

---
+++ src/conditional.php
@@ -0,0 +1,20 @@
+<?php
+
+$number = 8;
+
+
+// If , elseif and else conditional
+if($number == 10)
+{
+   echo "It's equal 10";
+}
+elseif($number <=9)
+{
+    echo "It's less or than 9";
+}
+else
+{
+    echo "It's bigger";
+}
+
+
