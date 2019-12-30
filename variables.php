7493d12025036d04b4ad421ac9284eb9055ac8cf
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-30 08:55:02 -0300

    Review of variables

---
+++ variables.php
@@ -0,0 +1,78 @@
+<?php
+
+// Variables
+
+$name = "Rondinele";
+$age = "37";
+
+
+echo "My name is $name, and my age is $age years old";
+
+//break line
+echo "<br>";
+echo"</hr>";
+echo "<br>";
+
+// modify data
+
+$name = "Beatriz";
+$age = 4;
+
+
+echo "My name is $name, and my age is $age years old";
+
+
+/*
+ Valid an invalid names of vaiables
+
+    Valid
+
+    $nome;
+    $_myname;
+    $car10;
+    $my_name;
+    $myName;
+
+
+    Invalid
+
+    $1212nome;
+    $car@#$%;
+
+    $my car;
+
+ */
+
+
+ // Case sensitive
+
+
+ echo"</hr>";
+
+ //same name but,are differents
+ 
+ $car = "Uno";
+ $CAR = "GOL";
+
+
+ // Variable of variables
+
+ $drink = "milk";
+
+ $$drink = "Swewet";
+
+ //break line
+echo "<br>";
+echo"</hr>";
+
+ echo "Variable $drink, and Variable of variables is $milk";
+
+ //break line
+echo "<br>";
+echo"</hr>";
+
+$destiny = "city";
+
+$$destiny = "Paris";
+ 
+ echo "Variable $destiny, and Variable of variables is $city";
