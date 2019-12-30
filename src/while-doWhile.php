327288857f197b5edae5ee83a381cc2f314434ea
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-30 12:31:50 -0300

    While and Do While

---
+++ src/while-doWhile.php
@@ -0,0 +1,19 @@
+<?php
+
+// While
+$count = 0;
+while($count <=10)
+{
+  echo "Count is $count <br>";
+  $count++;
+}
+
+echo"<hr>";
+
+//Do While
+$count = 0;
+
+do{
+    echo "Count is $count <br>";
+    $count++;
+}while($count <=10);
