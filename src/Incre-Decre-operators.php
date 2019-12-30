ee1fd75d45a5c048f4fbdc4a4dcfff90c3d7fb54
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-30 11:51:47 -0300

    increment and  decrement

---
+++ src/Incre-Decre-operators.php
@@ -0,0 +1,29 @@
+<?php
+
+$value = 20;
+
+//pre-increment
+echo  ++$value;
+echo"<hr>";
+echo $value;
+echo"<hr>";
+
+
+
+//pos-increment
+echo  $value++;
+echo"<hr>";
+echo $value;
+echo"<hr>";
+
+//pre-decrement
+echo  --$value;
+echo"<hr>";
+echo $value;
+echo"<hr>";
+
+//pos-decrement
+echo  $value--;
+echo"<hr>";
+echo $value;
+echo"<hr>";
