0613ab489ce187d8abf32c9e0c019dc1a41668cc
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-30 09:16:11 -0300

    Data Type Array

--- src/data-types.php
+++ src/data-types.php
@@ -3,23 +3,20 @@
 // String
 
 // double quotes
-$name  =78;
+$name  = 78;
 // single quotes
 $Name  = 'Rondinele';
 
 var_dump($name);
 var_dump($Name);
 
-if(is_string($name) || is_string($Name))
-{
-   echo" This variables is a string !!";
-}
-else
-{
-    echo" This variables is not a string !!";
+if (is_string($name) || is_string($Name)) {
+    echo " This variables is a string !!";
+} else {
+    echo " This variables is not a string !!";
 }
 
-echo"<hr>";
+echo "<hr>";
 
 
 // int
@@ -27,45 +24,43 @@ echo"<hr>";
 $age = 27;
 
 var_dump($age);
-if(is_int($age))
-{
-   echo" This variables is a integer !!";
-}
-else
-{
-    echo" This variables is not a integer !!";
+if (is_int($age)) {
+    echo " This variables is a integer !!";
+} else {
+    echo " This variables is not a integer !!";
 }
 
-echo"<hr>";
+echo "<hr>";
 
 // float
 
 $height = 1.66;
 
 var_dump($height);
-if(is_float($height))
-{
-   echo" This variables is a float number !!";
-}
-else
-{
-    echo" This variables is not a float number !!";
+if (is_float($height)) {
+    echo " This variables is a float number !!";
+} else {
+    echo " This variables is not a float number !!";
 }
 
-echo"<hr>";
+echo "<hr>";
 
 // boolean  
 
 $admin = true;
 
 var_dump($admin);
-if(is_bool($admin))
-{
-   echo" This variables is a boolean value !!";
-}
-else
-{
-    echo" This variables is not a boolean value !!";
+if (is_bool($admin)) {
+    echo " This variables is a boolean value !!";
+} else {
+    echo " This variables is not a boolean value !!";
 }
 
-echo"<hr>";
+echo "<hr>";
+
+// Array  
+$cars = array("Gol", "Uno", "Celta", 12, 2.0, true);
+
+var_dump($cars);
+
+echo "<hr>";
