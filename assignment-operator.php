3c73bf6514a780caa053417b1b701bce4e4f5c8d
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-30 11:58:45 -0300

    assignment operator

---
+++ assignment-operator.php
@@ -0,0 +1,40 @@
+<?php
+
+//assignment operator
+
+$a = 10 ;
+$b =5;
+
+echo"<hr>";
+$a += $b; // $a = $a + $b;
+echo $a;
+echo"<hr>";
+
+
+echo"<hr>";
+$a += $b; // $a = $a + $b;
+echo $a;
+echo"<hr>";
+
+
+echo"<hr>";
+$a -= $b; // $a = $a - $b;
+echo $a;
+echo"<hr>";
+
+
+echo"<hr>";
+$a *= $b; // $a = $a * $b;
+echo $a;
+echo"<hr>";
+
+
+echo"<hr>";
+$a /= $b; // $a = $a / $b;
+echo $a;
+echo"<hr>";
+
+echo"<hr>";
+$a %= $b; // $a = $a % $b;
+echo $a;
+echo"<hr>";
