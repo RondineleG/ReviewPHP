8d336a411d107eabda247f9dd0d7685cf47ee6d9
Rondinele Guimarães <rondineleg@edu.univali.br>
2020-01-02 09:55:40 -0300

    require

--- src/require-include.php
+++ src/require-include.php
@@ -6,7 +6,10 @@ echo"Show file quotes.php  using include function<br>";
 include 'quotes.php';
  echo"<hr>";
 
-
  echo"Show file data-types.php  using include_one function<br>";
 include_once 'data-types.php';
+ echo"<hr>";
+
+ echo"Show file config.php  using require function<br>";
+require 'config.php';
  echo"<hr>";
